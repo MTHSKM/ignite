cedec6a939b55c0ec6eff9559ca018c780508649
MTHSKM <105982883+MTHSKM@users.noreply.github.com>
2023-08-18 17:11:40 -0300

    Delete src/modules/cars directory

--- src/modules/cars/useCases/createSpecification/index.ts
+++
@@ -1,9 +0,0 @@
-import { SpecificationRepository } from "../../repositories/implementations/Specificationrepository";
-import { CreateSpecificationController } from "./CreateSpecificationController";
-import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
-
-const createSpecificationRepository = SpecificationRepository.getInstance()
-const createSpecificationUseCase = new CreateSpecificationUseCase(createSpecificationRepository)
-const createSpecificationController = new CreateSpecificationController(createSpecificationUseCase)
-
-export { createSpecificationController }
--- src/modules/cars/useCases/listCategories/index.ts
+++
@@ -1,9 +0,0 @@
-import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
-import { ListCategorieController } from "./ListCategorieController";
-import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
-
-const categoriesRepository = CategoriesRepository.getInstance()
-const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository)
-const listCategoriesController = new ListCategorieController(listCategoriesUseCase)
-
-export { listCategoriesController }
